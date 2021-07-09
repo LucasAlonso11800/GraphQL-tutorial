b026cab3be99998a8d83716fbd6bbd0c57edddbe
Lucas <lucasalonso11800@gmail.com>
2021-07-09 18:49:58 -0300

    Connected to Db

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema/schema');
+require('dotenv').config();
 
 const app = express();
+
+const MONGO_URI = process.env.MONGO_URI;
+
+//DB
+mongoose.connect(MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}, () => console.log('Connected to DB'));
 
 app.use('/graphql', graphqlHTTP({
     schema,
